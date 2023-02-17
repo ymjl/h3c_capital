dd9940ac17531256c9842a527582668a7bccb176
Vonki <nswanqi@gmail.com>
2023-02-17 08:01:40 +0800

    add cal fund

--- fund_cost_and_return.js
+++ fund_cost_and_return.js
@@ -19,11 +19,12 @@ function generatePlot() {
   }
 
   // 计算数据
-  var return_rates = Array.from(Array(Math.floor(6/step_size) - Math.floor(baseline_return/step_size)), (x, i) => (i + Math.floor(baseline_return/step_size)) * step_size);  // 基金收益率列表
+  var return_rates = Array.from(Array(Math.floor(6/step_size)).keys(), x => x * step_size);   // 基金收益率列表
   var management_fees = Array(return_rates.length).fill(fund_size * management_fee_rate);  // 管理费用列表
   var performance_fees = return_rates.map(r => fund_size * (r - baseline_return) * performance_fee_rate(r));  // 绩效费用列表
   var total_fees = performance_fees.map((p, i) => p + management_fees[i]);  // 总费用列表
   var investor_returns = return_rates.map((r, i) => fund_size * r - total_fees[i]);  // 投资人收益列表
+  var fund_returns = return_rates.map((r, i) => fund_size * r + management_fees[i] - investor_returns[i]); // 基金收益列表
 
   // 创建 DataFrame
   var df = {
@@ -34,19 +35,21 @@ function generatePlot() {
     '绩效费用': performance_fees,
     '管理费用': management_fees,
     '总费用': total_fees,
-    '投资人收益': investor_returns
+    '投资人收益': investor_returns,
+    '基金收益': fund_returns
   };
 
   // 可视化数据
   var data = [
+    { x: df['基金收益率'], y: df['投资人收益'], name: '投资人收益' },
     { x: df['基金收益率'], y: df['管理费用'], name: '管理费用' },
     { x: df['基金收益率'], y: df['绩效费用'], name: '绩效费用' },
     { x: df['基金收益率'], y: df['总费用'], name: '总费用' },
-    { x: df['基金收益率'], y: df['投资人收益'], name: '投资人收益' }
+    { x: df['基金收益率'], y: df['基金收益'], name: '基金收益' },
   ];
 
   var layout = {
-    title: '基金费用和投资人收益计算',
+    title: '投资人收益计算',
     xaxis: {
       title: '基金收益率',
       tickformat: ',.0%',
