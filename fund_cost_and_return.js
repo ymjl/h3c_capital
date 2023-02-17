1a07c17846a85b04f819649f636c693ed25ac184
Vonki <nswanqi@gmail.com>
2023-02-17 09:01:58 +0800

    add cal fund

--- fund_cost_and_return.js
+++ fund_cost_and_return.js
@@ -4,24 +4,32 @@ function generatePlot() {
   var step_size = document.getElementById('step-size').value;
   var baseline_return = document.getElementById('baseline-return').value;
   var management_fee_rate = document.getElementById('management-fee-rate').value;
-
+  var performance_fee_rates = [
+  parseFloat(document.getElementById('performance-fee-rate-1').value),
+  parseFloat(document.getElementById('performance-fee-rate-2').value),
+  parseFloat(document.getElementById('performance-fee-rate-3').value)
+  ]; 
   // 定义绩效费率函数
   function performance_fee_rate(return_rate) {
     if (return_rate < 0.6) {
       return 0;
     } else if (return_rate < 2) {
-      return (return_rate - 0.6) * 0.25;
+      return (return_rate - baseline_return) * performance_fee_rates[0];
     } else if (return_rate < 4) {
-      return 0.35 + (return_rate - 2) * 0.1;
+      return 1.4 * performance_fee_rates[0] + (return_rate - 2) * performance_fee_rates[1];
     } else {
-      return 0.55;
+      
+      return (1.4 * performance_fee_rates[0] + 2 * performance_fee_rates[1] + (return_rate - 4)* performance_fee_rates[2]);
     }
   }
 
   // 计算数据
-  var return_rates = Array.from(Array(Math.floor(6/step_size)).keys(), x => x * step_size);   // 基金收益率列表
+  var return_rates = Array.from(Array(Math.floor(6/step_size) - Math.floor(0/step_size)), (x, i) => (i + Math.floor(0/step_size)) * step_size); 
+  
   var management_fees = Array(return_rates.length).fill(fund_size * management_fee_rate);  // 管理费用列表
-  var performance_fees = return_rates.map(r => fund_size * (r - baseline_return) * performance_fee_rate(r));  // 绩效费用列表
+
+  var performance_fees = return_rates.map(r => fund_size * performance_fee_rate(r));  // 绩效费用列表
+
   var total_fees = performance_fees.map((p, i) => p + management_fees[i]);  // 总费用列表
   var investor_returns = return_rates.map((r, i) => fund_size * r - total_fees[i]);  // 投资人收益列表
  // var fund_returns = return_rates.map((r, i) => fund_size * r + management_fees[i] - investor_returns[i]); // 基金收益列表
@@ -63,7 +71,3 @@ function generatePlot() {
   // 使用 Plotly 绘制折线图
   Plotly.newPlot('plot', data, layout);
 }
-
-// 绑定“生成图表”按钮的点击事件
-var generateButton = document.getElementById('generate-button');
-generateButton.addEventListener('click', generatePlot);
